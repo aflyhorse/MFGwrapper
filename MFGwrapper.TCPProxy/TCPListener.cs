eeccd9ef13eb86bcdf0805bf9ff2dd2e0ee4be5e
Chen Chen <aflyhorse@hotmail.com>
2015-10-08 17:08:02 +0800

    create tcpsender

---
+++ MFGwrapper.TCPProxy/TCPListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFGwrapper.TCPProxy
+{
+    class TCPListener
+    {
+        public System.Net.IPAddress BindingIP
+        { get; }
+
+        public int BindingPort
+        { get; }
+
+        private System.Net.Sockets.TcpListener tcplistener;
+        private System.Net.Sockets.NetworkStream ns;
+        private System.IO.StreamReader sr;
+
+        public TCPListener(System.Net.IPAddress BindingIP, int BindingPort)
+        {
+            this.BindingIP = BindingIP;
+            this.BindingPort = BindingPort;
+            tcplistener = new System.Net.Sockets.TcpListener(BindingIP, BindingPort);
+        }
+    }
+}
---
+++ MFGwrapper.TCPProxy/TCPSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFGwrapper.TCPProxy
+{
+    class TCPSender
+    {
+        public System.Net.IPAddress UpstreamIP
+        { get; }
+
+        public int UpstreamPort
+        { get; }
+
+        private System.Net.Sockets.TcpClient tcpclient;
+        private System.Net.Sockets.NetworkStream ns;
+        private System.IO.StreamWriter sw;
+
+        public TCPSender(System.Net.IPAddress UpstreamIP, int UpstreamPort)
+        {
+            this.UpstreamIP = UpstreamIP;
+            this.UpstreamPort = UpstreamPort;
+            tcpclient = new System.Net.Sockets.TcpClient(new System.Net.IPEndPoint(UpstreamIP, UpstreamPort));
+            ns = tcpclient.GetStream();
+            sw = new System.IO.StreamWriter(ns);
+            sw.AutoFlush = true;
+        }
+
+        ~TCPSender()
+        {
+            sw.Close();
+            ns.Close();
+            tcpclient.Close();
+        }
+
+        public void Send(string str)
+        {
+            sw.Write(str);
+        }
+
+    }
+}
