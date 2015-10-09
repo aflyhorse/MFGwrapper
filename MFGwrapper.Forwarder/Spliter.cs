24c00534ebed70a5237ecd4eb192d0d7cd9a4d28
Chen Chen <aflyhorse@hotmail.com>
2015-10-09 11:27:09 +0800

    create a simple forward proxy

--- MFGwrapper.Forwarder/Spliter.cs
+++ MFGwrapper.Forwarder/Spliter.cs
@@ -7,5 +7,28 @@ namespace MFGwrapper.Forwarder
 {
     public class Spliter
     {
+        public int UpstreamPort
+        { get; }
+        public int MGFPort
+        { get; }
+        public int ListenPort
+        { get; }
+
+        public Spliter(int UpstreamPort, int MGFPort, int ListenPort)
+        {
+            this.UpstreamPort = UpstreamPort;
+            this.MGFPort = MGFPort;
+            this.ListenPort = ListenPort;
+            Fiddler.FiddlerApplication.SetAppDisplayName("MFGwrapper");
+            Fiddler.FiddlerApplication.BeforeRequest += FiddlerApplication_BeforeRequest;
+            Fiddler.FiddlerApplication.Startup(ListenPort,
+                Fiddler.FiddlerCoreStartupFlags.Default & ~Fiddler.FiddlerCoreStartupFlags.DecryptSSL);
+        }
+
+        private void FiddlerApplication_BeforeRequest(Fiddler.Session oSession)
+        {
+            oSession.bBufferResponse = false;
+            oSession["X-OverrideGateway"] = "localhost:" + UpstreamPort;
+        }
     }
 }
