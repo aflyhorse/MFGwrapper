aa5d2596cd44967bd98084b22d7d5f822d9f27bd
Chen Chen <aflyhorse@hotmail.com>
2016-01-04 17:10:12 +0800

    add port checker.

---
+++ MFGwrapper.Forwarder/Nettools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MFGwrapper.Forwarder
+{
+    public static class Nettools
+    {
+        public static bool isPortInUse(int port)
+        {
+            bool inUse = false;
+            var IPEndPoints = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners();
+            foreach (var IPEndPoint in IPEndPoints)
+            {
+                if (IPEndPoint.Port == port)
+                {
+                    inUse = true;
+                    break;
+                }
+            }
+            return inUse;
+        }
+    }
+}
